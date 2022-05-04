f6c8b779786be89b1951ac61b2ef3d3dddb5a34a
krismina la <krismina.la@gmail.com>
2022-05-04 03:51:04 +0000

    add store, rate validation, error message print

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -12,25 +12,44 @@ puts "----------"
 # Your code goes here ...
 
 class Employee < ActiveRecord::Base
-  validate :first_name_check, :last_name_check, :rate_check
+  validate :first_name_check, :last_name_check, :rate_check, :store_check, :display_error
 
   def first_name_check
-    if !self.first_name.present?
+    unless self.first_name.present?
       self.errors.add(:first_name, "required")
-
-      puts "error: #{self.errors.messages}"
     end
   end
 
   def last_name_check
-    if !self.last_name.present?
+    unless self.last_name.present?
       self.errors.add(:last_name, "required")
+    end
+  end
 
-      puts "error: #{self.errors.messages}"
+  def rate_check
+    unless self.hourly_rate.is_a? Integer
+      self.errors.add(:hourly_rate, "needs to be an integer")
+    end
+
+    unless (40..200).include? self.hourly_rate
+      self.errors.add(:hourly_rate, "must be inbetween 40 to 200 inclusively")
     end
 
   end
 
+  def store_check
+    if self.store_id.nil?
+      self.errors.add(:store_id, "missing or invalid")
+    end
+  end
+
+  # Display error message if there is any
+  def display_error
+    puts "error: #{self.errors.messages}"
+  end
+
 end
 
-@store1.employees.create(first_name: "Jack",  hourly_rate: 29)
+@store1.employees.create(first_name: "Jack", last_name: "Ray", hourly_rate: 50)
+
+Employee.create(first_name: "Jack", last_name: "Ray", hourly_rate: 50)
