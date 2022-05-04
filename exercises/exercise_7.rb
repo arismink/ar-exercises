e023f856072d07892e15e285c3004411462fad1c
krismina la <krismina.la@gmail.com>
2022-05-04 03:34:13 +0000

    add first name, last name validation to employee

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,27 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validate :first_name_check, :last_name_check, :rate_check
+
+  def first_name_check
+    if !self.first_name.present?
+      self.errors.add(:first_name, "required")
+
+      puts "error: #{self.errors.messages}"
+    end
+  end
+
+  def last_name_check
+    if !self.last_name.present?
+      self.errors.add(:last_name, "required")
+
+      puts "error: #{self.errors.messages}"
+    end
+
+  end
+
+end
+
+@store1.employees.create(first_name: "Jack",  hourly_rate: 29)
