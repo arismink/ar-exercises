2269b10365a0f3a6254386d5fcb2170873f2b839
krismina la <krismina.la@gmail.com>
2022-05-04 02:59:47 +0000

    populate employees table

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,6 +5,8 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# Load store and assign to instance variable
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
 @store3 = Store.find_by(id: 3)
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,22 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create([
+  {first_name: "Sally", last_name: "Bob", hourly_rate: 40},
+  {first_name: "Joe", last_name: "Bob", hourly_rate: 30},
+  {first_name: "Ray", last_name: "Jr", hourly_rate: 90},
+  {first_name: "Tally", last_name: "Mob", hourly_rate: 140},
+])
+
+@store2.employees.create([
+  {first_name: "Tina", last_name: "Fey", hourly_rate: 50},
+  {first_name: "James", last_name: "Dean", hourly_rate: 40},
+  {first_name: "Lisa", last_name: "Bob", hourly_rate: 90},
+])
